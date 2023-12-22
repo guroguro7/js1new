04f61ff2b79d5f29c9c89f7ab747b2ea0061a02f
guroguro7 <148255413+guroguro7@users.noreply.github.com>
2023-12-22 19:35:40 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const x = 100;
+
+if (x < 50){
+     console.log("naklebia 50-ze")
+}else if (x>20){
+    console.log("metia 20-ze")
+} else {
+    console.log(false);
+} 
+
+const userName = "mariam";
+
+
+if(userName) {
+    console.log("true");
+}else {
+    console.log("false");
+
+} 
+
+
+const name ="mariam";
+
+switch (userName){
+    case 'mariam':
+        console.log(true);
+        break;
+        case "else" :
+
+   console.log("false") 
+}
+
